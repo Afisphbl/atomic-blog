5c190608a3cd0623211c9f05e537af6bbeca34d8
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-24 09:31:04 +0300

    feat: implement PostsContext for managing blog posts state

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
 import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import { PostsProvider, usePosts } from "./context/PostContext";
 import CreateRandomPost from "./components/CreateRandomPost/CreateRandomPost";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => CreateRandomPost({ faker })),
-  );
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isFakeDark, setIsFakeDark] = useState(false);
-
-  // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase()),
-        )
-      : posts;
-
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
-
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-  useEffect(
-    function () {
-      document.documentElement.classList.toggle("fake-dark-mode");
-    },
-    [isFakeDark],
-  );
+  const {
+    posts,
+    handleAddPost,
+    handleClearPosts,
+    searchQuery,
+    searchedPosts,
+    setSearchQuery,
+    isFakeDark,
+    setIsFakeDark,
+  } = usePosts();
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
+    <PostsProvider>
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
 
-      <Header
-        posts={searchedPosts}
-        onClearPosts={handleClearPosts}
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-      <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive onAddPost={handleAddPost} />
-      <Footer />
-    </section>
+        <Header
+          posts={searchedPosts}
+          onClearPosts={handleClearPosts}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <Main posts={searchedPosts} onAddPost={handleAddPost} />
+        <Archive onAddPost={handleAddPost} />
+        <Footer />
+      </section>
+    </PostsProvider>
   );
 }
 
---
+++ src/context/PostContext.jsx
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import CreateRandomPost from "../components/CreateRandomPost/CreateRandomPost";
+
+const PostsContext = createContext();
+
+export const PostsProvider = ({ children }) => {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => CreateRandomPost({ faker })),
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase()),
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark],
+  );
+
+  const value = {
+    posts,
+    handleAddPost,
+    handleClearPosts,
+    searchQuery,
+    searchedPosts,
+    setSearchQuery,
+    isFakeDark,
+    setIsFakeDark,
+  };
+
+  return (
+    <PostsContext.Provider value={value}>{children}</PostsContext.Provider>
+  );
+};
+
+export const usePosts = () => {
+  return useContext(PostsContext);
+};
