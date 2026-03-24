7de384ea8099988dae701f8bb842164b7adb720f
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-24 09:59:08 +0300

    feat: implement useDebounce hook and integrate it into SearchPosts component for improved search functionality

--- src/components/SearchPost/SearchPost.jsx
+++ src/components/SearchPost/SearchPost.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import { usePosts } from "../../context/PostContext";
+import useDebounce from "../Debounce/useDebounce";
 
 export default function SearchPosts() {
   const { searchQuery, setSearchQuery } = usePosts();
+  const [query, setQuery] = useState(searchQuery);
+  const debouncedQuery = useDebounce(query, 500);
+
+  useEffect(() => {
+    setSearchQuery(debouncedQuery);
+  }, [debouncedQuery, setSearchQuery]);
+
+  useEffect(() => {
+    setQuery(searchQuery);
+  }, [searchQuery]);
 
   return (
     <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
       placeholder="Search posts..."
     />
   );
