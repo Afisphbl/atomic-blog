e281fbb76bdf166df7015c92bc2489646c4c0512
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-24 09:23:53 +0300

    feat: initialize atomic blog project with essential files and components
    - Added package.json with dependencies and scripts for React app
    - Created favicon.ico for the application
    - Set up index.html as the main HTML file
    - Developed App component to manage posts and search functionality
    - Implemented Test component to demonstrate performance with a large list
    - Established index.js as the entry point for the React application
    - Added style.css for basic styling and layout

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import CreateRandomPost from "./components/CreateRandomPost/CreateRandomPost";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
+function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => CreateRandomPost({ faker })),
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase()),
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark],
+  );
+
+  return (
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+
+      <Header
+        posts={searchedPosts}
+        onClearPosts={handleClearPosts}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Archive onAddPost={handleAddPost} />
+      <Footer />
+    </section>
+  );
+}
+
+function Archive({ onAddPost }) {
+  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  const [posts] = useState(() =>
+    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+    Array.from({ length: 10000 }, () => CreateRandomPost({ faker })),
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+
+export default App;
